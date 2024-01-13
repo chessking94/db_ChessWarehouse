af871bbd15fa0c171f9b306587098576c72d482d
developer <developer@hunthome.dev>
2024-01-13 13:26:50 -0600

    Remove custom error messages; hard-code text since it's process-specific (sys.messages did not migrate to new server)

--- db_ChessWarehouse/dbo/Stored Procedures/LoadGameData.sql
+++ db_ChessWarehouse/dbo/Stored Procedures/LoadGameData.sql
@@ -7,7 +7,7 @@ DECLARE @filetypeid smallint = 1 --hard code file type; one proc, one type
 DECLARE @dir varchar(200)
 DECLARE @ext varchar(10)
 DECLARE @cmd varchar(500)
-DECLARE @err_msg int = -1
+DECLARE @err_msg nvarchar(100)
 
 DECLARE @file_ct int
 DECLARE @filename varchar(100)
@@ -17,12 +17,12 @@ DECLARE @rec_ct int
 DECLARE @fld varchar(20)
 DECLARE @err_ct int
 
---get a listing of files to import
-IF (OBJECT_ID('tempdb..#directory') IS NOT NULL) DROP TABLE #directory
-CREATE TABLE #directory (content varchar(1000))
+--get a listing of files to import; use file type ID to differentiate temp tables across procs
+IF (OBJECT_ID('tempdb..#directory1') IS NOT NULL) DROP TABLE #directory1
+CREATE TABLE #directory1 (content varchar(1000))
 
-IF (OBJECT_ID('tempdb..#pendingfiles') IS NOT NULL) DROP TABLE #pendingfiles
-CREATE TABLE #pendingfiles (filename varchar(200))
+IF (OBJECT_ID('tempdb..#pendingfiles1') IS NOT NULL) DROP TABLE #pendingfiles1
+CREATE TABLE #pendingfiles1 (filename varchar(200))
 
 SET @dir = dbo.GetSettingValue('FileProcessing Directory')
 IF RIGHT(@dir, 1) <> '\' SET @dir = @dir + '\'
@@ -30,27 +30,28 @@ SET @dir = @dir + @@SERVERNAME + '\Import\GameData\'
 
 SET @cmd = 'DIR "' + @dir + '"'
 
-INSERT INTO #directory
+INSERT INTO #directory1
 EXEC master.dbo.xp_cmdshell @cmd
 
 SELECT @ext = FileExtension FROM FileTypes WHERE FileTypeID = @filetypeid
-INSERT INTO #pendingfiles
-SELECT LTRIM(RTRIM(SUBSTRING(content, 40, LEN(content)))) FROM #directory WHERE (CASE WHEN content LIKE '%.%' THEN REVERSE(LEFT(REVERSE(content), CHARINDEX('.', REVERSE(content)) - 1)) ELSE NULL END) = @ext
+INSERT INTO #pendingfiles1
+SELECT LTRIM(RTRIM(SUBSTRING(content, 40, LEN(content)))) FROM #directory1 WHERE (CASE WHEN content LIKE '%.%' THEN REVERSE(LEFT(REVERSE(content), CHARINDEX('.', REVERSE(content)) - 1)) ELSE NULL END) = @ext
 
-SELECT @file_ct = COUNT(filename) FROM #pendingfiles
+SELECT @file_ct = COUNT(filename) FROM #pendingfiles1
 IF @file_ct = 0
 BEGIN
-	RAISERROR('No files found to import', 1, 1)
+	SET @err_msg = 'No files found to import'
+	RAISERROR(@err_msg, 1, 1)
 	RETURN @err_msg
 END
 
 EXEC dbo.DeleteStagedGameData @errors = 0
-SET @err_msg = 0
+SET @err_msg = NULL
 
 --all good, begin file processing loop
 WHILE @file_ct > 0
 BEGIN
-	SELECT TOP 1 @filename = filename FROM #pendingfiles
+	SELECT TOP 1 @filename = filename FROM #pendingfiles1
 	SET @dte = GETDATE()
 
 	TRUNCATE TABLE stage.BulkInsertGameData
@@ -73,18 +74,18 @@ BEGIN
 	EXEC dbo.DupCheckStagedGames
 
 	SELECT @err_ct = COUNT(Errors) FROM stage.Games WHERE Errors IS NOT NULL
-	IF @err_ct > 0 SET @err_msg = 50001
+	IF @err_ct > 0 SET @err_msg = 'Unable to validate Game record(s)'
 
-	IF @err_msg = 0
+	IF @err_msg IS NULL
 	BEGIN
 		--stage and preprocess move data
 		EXEC dbo.StageMoves
 		EXEC dbo.FormatMoveData
 
 		SELECT @err_ct = COUNT(Errors) FROM stage.Moves WHERE Errors IS NOT NULL
-		IF @err_ct > 0 SET @err_msg = 50002
+		IF @err_ct > 0 SET @err_msg = 'Unable to validate Move record(s)'
 
-		IF @err_msg = 0
+		IF @err_msg IS NULL
 		BEGIN
 			--stage new dimension data
 			EXEC dbo.InsertNewEvents
@@ -118,20 +119,20 @@ BEGIN
 	SET DateCompleted = GETDATE(), Errors = @err_ct
 	WHERE FileID = @fhid
 
-	IF @err_msg = 0
+	IF @err_msg IS NULL
 	BEGIN
 		SET @cmd = 'MOVE "' + @dir + + @filename + '" "' + @dir + 'Archive\' + @filename + '"'
 		EXEC master.dbo.xp_cmdshell @cmd
 	END
 
-	DELETE FROM #pendingfiles WHERE filename = @filename
-	SELECT @file_ct = COUNT(filename) FROM #pendingfiles
+	DELETE FROM #pendingfiles1 WHERE filename = @filename
+	SELECT @file_ct = COUNT(filename) FROM #pendingfiles1
 END
 
-DROP TABLE #directory
-DROP TABLE #pendingfiles
+DROP TABLE #directory1
+DROP TABLE #pendingfiles1
 
-IF @err_msg > 0
+IF @err_msg IS NOT NULL
 BEGIN
 	RAISERROR(@err_msg, 16, 1)
 END
