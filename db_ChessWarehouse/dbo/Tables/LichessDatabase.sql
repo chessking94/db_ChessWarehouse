94ddd124c0771c916bd47a1d43e6bf18f6f5c26f
Ethan Hunt <chessking94@gmail.com>
2024-06-12 22:04:07 -0500

    Add table to log info about parsed Lichess database files

---
+++ db_ChessWarehouse/dbo/Tables/LichessDatabase.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[LichessDatabase]
+(
+	[Filename] VARCHAR(50) NOT NULL, 
+    [Decompression_Start] DATETIME NULL, 
+    [Decompression_End] DATETIME NULL, 
+    [ErrorLog_Start] DATETIME NULL, 
+    [ErrorLog_End] DATETIME NULL, 
+    [2200_Start] DATETIME NULL, 
+    [2200_End] DATETIME NULL, 
+    [Corr_Start] DATETIME NULL, 
+    [Corr_End] DATETIME NULL, 
+    [Bullet_2200] INT NULL, 
+    [Blitz_2200] INT NULL, 
+    [Rapid_2200] INT NULL, 
+    [Classical_2200] INT NULL, 
+    [Corr_All] INT NULL, 
+    [Corr_Additional] INT NULL,
+    CONSTRAINT [PK_LichessDatabase] PRIMARY KEY CLUSTERED ([Filename] ASC)
+)
