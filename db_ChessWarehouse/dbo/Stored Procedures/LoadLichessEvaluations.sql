5c89566fd5f4b2caeabd3e8c7f0a2f31ac498f6c
Ethan Hunt <chessking94@gmail.com>
2024-12-06 22:17:40 -0600

    Add Try/Catch to assist in logging, add missing role to automation_user

--- db_ChessWarehouse/dbo/Stored Procedures/LoadLichessEvaluations.sql
+++ db_ChessWarehouse/dbo/Stored Procedures/LoadLichessEvaluations.sql
@@ -13,61 +13,69 @@ AS
 	after processing in a clean-up method.
 */
 
-DECLARE @filetypeid SMALLINT = 2 --hard code file type; one proc, one type
-DECLARE @dir VARCHAR(200)
-DECLARE @cmd VARCHAR(500)
-DECLARE @fhid INT
-DECLARE @err_ct INT
+BEGIN TRY
+	DECLARE @filetypeid SMALLINT = 2 --hard code file type; one proc, one type
+	DECLARE @dir VARCHAR(200)
+	DECLARE @cmd VARCHAR(500)
+	DECLARE @fhid INT
+	DECLARE @err_ct INT
 
---confirm the file hasn't been processed before; assume it has if there is an entry with 0 errors in the history
-IF EXISTS (SELECT FileID FROM dbo.FileHistory WHERE FileTypeID = @filetypeid AND Filename = @piFileName AND Errors = 0)
-BEGIN
-	SET @poErrorMessage = 'File previously processed'
+	--confirm the file hasn't been processed before; assume it has if there is an entry with 0 errors in the history
+	IF EXISTS (SELECT FileID FROM dbo.FileHistory WHERE FileTypeID = @filetypeid AND Filename = @piFileName AND Errors = 0)
+	BEGIN
+		SET @poErrorMessage = 'File previously processed'
+		--;THROW 50000, @poErrorMessage, 1
+		RETURN -1
+	END
+
+	--reaching here means file hasn't been processed before, continue
+	SET @dir = dbo.GetSettingValue('FileProcessing Directory')
+	IF RIGHT(@dir, 1) <> '\' SET @dir = @dir + '\'
+	SET @dir = @dir + @@SERVERNAME + '\Import\LichessEvaluations\'
+
+	EXEC dbo.DeleteStagedLichessEvaluations @errors = 0
+	TRUNCATE TABLE lake.LichessEvaluations
+
+	TRUNCATE TABLE stage.BulkInsertLichessEvaluations
+	SET @cmd = 'BULK INSERT stage.BulkInsertLichessEvaluations FROM ''' + @dir + @piFileName + ''' WITH (BATCHSIZE = 5000, FIELDTERMINATOR = ''\t'', ROWTERMINATOR = ''\n'', TABLOCK)'
+	EXECUTE (@cmd)
+
+	--log file record
+	INSERT INTO dbo.FileHistory (FileTypeID, Filename, DateStarted, Records)
+	SELECT @filetypeid, @piFileName, GETDATE(), (SELECT COUNT(FEN) FROM stage.BulkInsertLichessEvaluations)
+
+	SET @fhid = @@IDENTITY
+
+	--stage and preprocess game data
+	EXEC dbo.StageLichessEvals @fileid = @fhid
+	EXEC dbo.FormatLichessEvals
+
+	SELECT @err_ct = COUNT(Errors) FROM stage.LichessEvaluations WHERE Errors IS NOT NULL
+	IF @err_ct > 0 SET @poErrorMessage = 'Unable to validate Evaluation(s)'
+
+	IF @poErrorMessage IS NULL
+	BEGIN
+		--finally insert new records
+		EXEC dbo.InsertLichessEvaluations
+	END
+
+	EXEC dbo.DeleteStagedLichessEvaluations @errors = @err_ct
+
+	UPDATE dbo.FileHistory
+	SET DateCompleted = GETDATE(), Errors = @err_ct
+	WHERE FileID = @fhid
+
+	IF @poErrorMessage IS NOT NULL
+	BEGIN
+		--;THROW 50000, @poErrorMessage, 1
+		RETURN -1
+	END
+
+	RETURN 0
+END TRY
+
+BEGIN CATCH
+	SET @poErrorMessage = '[' + CAST(ERROR_LINE() AS VARCHAR) + '] ' + ERROR_MESSAGE()
 	--;THROW 50000, @poErrorMessage, 1
 	RETURN -1
-END
-
---reaching here means file hasn't been processed before, continue
-SET @dir = dbo.GetSettingValue('FileProcessing Directory')
-IF RIGHT(@dir, 1) <> '\' SET @dir = @dir + '\'
-SET @dir = @dir + @@SERVERNAME + '\Import\LichessEvaluations\'
-
-EXEC dbo.DeleteStagedLichessEvaluations @errors = 0
-TRUNCATE TABLE lake.LichessEvaluations
-
-TRUNCATE TABLE stage.BulkInsertLichessEvaluations
-SET @cmd = 'BULK INSERT stage.BulkInsertLichessEvaluations FROM ''' + @dir + @piFileName + ''' WITH (BATCHSIZE = 5000, FIELDTERMINATOR = ''\t'', ROWTERMINATOR = ''\n'', TABLOCK)'
-EXECUTE (@cmd)
-
---log file record
-INSERT INTO dbo.FileHistory (FileTypeID, Filename, DateStarted, Records)
-SELECT @filetypeid, @piFileName, GETDATE(), (SELECT COUNT(FEN) FROM stage.BulkInsertLichessEvaluations)
-
-SET @fhid = @@IDENTITY
-
---stage and preprocess game data
-EXEC dbo.StageLichessEvals @fileid = @fhid
-EXEC dbo.FormatLichessEvals
-
-SELECT @err_ct = COUNT(Errors) FROM stage.LichessEvaluations WHERE Errors IS NOT NULL
-IF @err_ct > 0 SET @poErrorMessage = 'Unable to validate Evaluation(s)'
-
-IF @poErrorMessage IS NULL
-BEGIN
-	--finally insert new records
-	EXEC dbo.InsertLichessEvaluations
-END
-
-EXEC dbo.DeleteStagedLichessEvaluations @errors = @err_ct
-
-UPDATE dbo.FileHistory
-SET DateCompleted = GETDATE(), Errors = @err_ct
-WHERE FileID = @fhid
-
-IF @poErrorMessage IS NOT NULL
-BEGIN
-	--;THROW 50000, @poErrorMessage, 1
-	RETURN -1
-END
-
-RETURN 0
+END CATCH
