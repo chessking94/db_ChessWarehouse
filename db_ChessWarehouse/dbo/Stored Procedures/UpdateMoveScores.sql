c3b6d77003b8b0a7f1574874aeb99100dd4b5236
eehunt <ethan@hunthome.dev>
2025-06-18 10:45:59 +0000

    Move trace update (#12)
    Only consider a move forced if over the threshold AND is played, if not played consider it a blunder and score it

--- db_ChessWarehouse/dbo/Stored Procedures/UpdateMoveScores.sql
+++ db_ChessWarehouse/dbo/Stored Procedures/UpdateMoveScores.sql
@@ -27,7 +27,7 @@ SET m.TraceKey = (
 		WHEN m.IsTablebase = 1 THEN 't'
 		WHEN (m.T1_Eval_POV IS NULL OR ABS(m.T1_Eval_POV) > CAST(s.Value AS decimal(5,2))) OR (m.Move_Eval_POV IS NULL OR ABS(m.Move_Eval_POV) > CAST(s.Value AS decimal(5,2))) THEN 'e'
 		WHEN cte.Position_Count > 1 THEN 'r'
-		WHEN m.T2_Eval IS NULL OR ABS(CAST(m.T1_Eval AS decimal(5,2)) - (CASE WHEN LEFT(m.T2_Eval, 1) = '#' THEN 100 ELSE CAST(m.T2_Eval AS decimal(5,2)) END)) > 2.00 THEN 'f'
+		WHEN m.T2_Eval IS NULL OR (ABS(CAST(m.T1_Eval AS decimal(5,2)) - (CASE WHEN LEFT(m.T2_Eval, 1) = '#' THEN 100 ELSE CAST(m.T2_Eval AS decimal(5,2)) END)) > 2.00 AND m.Move_Rank = 1) THEN 'f'
 		WHEN m.Move_Rank = 1 THEN 'M'
 		ELSE '0'
 	END
