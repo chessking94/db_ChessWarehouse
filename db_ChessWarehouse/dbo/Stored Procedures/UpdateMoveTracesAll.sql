dcbcf14041ea70c2aaede38bf771b76600215ff0
eehunt <ethan@hunthome.dev>
2025-06-29 02:13:06 +0000

    Change the way eliminated moves are determined, don't want to eliminate moves when the "best" eval is over the threshold but the played move is not (#13)

--- db_ChessWarehouse/dbo/Stored Procedures/UpdateMoveTracesAll.sql
+++ db_ChessWarehouse/dbo/Stored Procedures/UpdateMoveTracesAll.sql
@@ -2,6 +2,11 @@
 
 AS
 
+--THIS PROCEDURE DOES A LOT AND WILL TAKE SOME TIME TO RUN, YOU HAVE BEEN WARNED
+
+--update lake.Moves.TraceKey
+DECLARE @ForcedMoveThreshold DECIMAL(5,2) = CAST(dbo.GetSettingValue(1) AS decimal(5,2))
+
 ;WITH cte AS (
 	SELECT
 	GameID,
@@ -15,13 +20,13 @@ AS
 UPDATE m
 SET m.TraceKey = (
 	CASE
-		WHEN m.IsTheory = 1 THEN 'b'
-		WHEN m.IsTablebase = 1 THEN 't'
-		WHEN m.T1_Eval_POV IS NULL OR ABS(m.T1_Eval_POV) > CAST(s.Value AS decimal(5,2)) THEN 'e'
-		WHEN cte.Position_Count > 1 THEN 'r'
-		WHEN m.T2_Eval IS NULL OR (ABS(CAST(m.T1_Eval AS decimal(5,2)) - (CASE WHEN LEFT(m.T2_Eval, 1) = '#' THEN 100 ELSE CAST(m.T2_Eval AS decimal(5,2)) END)) > 2.00 AND m.Move_Rank = 1) THEN 'f'
-		WHEN m.Move_Rank = 1 THEN 'M'
-		ELSE '0'
+		WHEN m.IsTheory = 1 THEN 'b'  --book moves
+		WHEN m.IsTablebase = 1 THEN 't'  --tablebase moves
+		WHEN ISNULL(ABS(m.T1_Eval_POV), 100) > CAST(s.Value AS decimal(5,2)) AND ISNULL(ABS(m.Move_Eval_POV), 100) > CAST(s.Value AS decimal(5,2)) THEN 'e'  --eval is considered clearly winning; both are are to allow really bad blunders to be scored
+		WHEN cte.Position_Count > 1 THEN 'r'  --repeated positions
+		WHEN m.T2_Eval IS NULL OR (ABS((CASE WHEN LEFT(m.T1_Eval, 1) = '#' THEN 100 ELSE CAST(m.T1_Eval AS decimal(5,2)) END) - (CASE WHEN LEFT(m.T2_Eval, 1) = '#' THEN 100 ELSE CAST(m.T2_Eval AS decimal(5,2)) END)) > @ForcedMoveThreshold AND m.Move_Rank = 1) THEN 'f'  --forced moves
+		WHEN m.Move_Rank = 1 THEN 'M'  --eval matches
+		ELSE '0'  --move must have been subpar
 	END
 )
 
@@ -35,3 +40,41 @@ JOIN cte ON
 	m.ColorID = cte.ColorID
 
 WHERE s.ID = 3
+
+--update lake.Moves.MoveScored
+UPDATE m
+SET m.MoveScored = t.Scored
+
+FROM lake.Moves m
+JOIN dim.Traces t ON
+	m.TraceKey = t.TraceKey
+JOIN lake.Games g ON
+	m.GameID = g.GameID
+
+--recalculate scores
+EXEC dbo.UpdateMoveScoresAll
+
+--recalculate fact tables
+EXEC dbo.InsertEventFacts
+EXEC dbo.InsertGameFacts
+EXEC dbo.InsertEvaluationFacts
+EXEC dbo.InsertEvaluationSplits
+
+--recalculate z-scores
+EXEC dbo.UpdateEventFacts
+EXEC dbo.UpdateGameFacts
+EXEC dbo.UpdateEvaluationFacts
+
+--recalculate rpt schema
+EXEC rpt.UpdateECOSummary
+EXEC rpt.UpdateEventSummary
+EXEC rpt.UpdateGameStatistics
+EXEC rpt.UpdateGameSummary
+EXEC rpt.UpdateOpponentSummary
+EXEC rpt.UpdateRatingSummary
+EXEC rpt.UpdateRoundSummary
+EXEC rpt.UpdateTimeControlSummary
+EXEC rpt.UpdateYearlyRoundStatistics
+EXEC rpt.UpdateYearlySummary
+EXEC rpt.UpdateYearlyTimeControlStatistics
+EXEC rpt.UpdateYearlyTStatistics
